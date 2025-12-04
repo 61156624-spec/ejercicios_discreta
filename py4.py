3e5542ed6028ef738f95d4e26fb1ec1f6bb81875
61156624-spec <61156624@continental.edu.pe>
2025-12-04 11:50:59 -0500

    variacion con repeticion

--- py4.py
+++ py4.py
@@ -1,22 +1,14 @@
 # permutacion
 # Programa: combinación C(n, k)
-
-def factorial(n):
-    resultado = 1
-    for i in range(1, n + 1):
-        resultado *= i
-    return resultado
-
 n = int(input("Ingresa n (total de elementos): "))
 k = int(input("Ingresa k (elementos que eliges): "))
 
-if k < 0 or k > n:
+if k < 0:
     print("k debe estar entre 0 y n.")
 else:
-    n_fact = factorial(n)
-    k_fact = factorial(n-k)
+    
     
 
-    p = n_fact / ((k_fact))
+    vr = n**k
 
-    print("P(", n, ",", k, ") =", p)
+    print("vr(", n, ",", k, ") =", vr)
